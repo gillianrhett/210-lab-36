5b4e8759278f2d076d29abf8954bd2ca69760a66
gillianrhett <gillianrhett@gmail.com>
2026-05-10 08:34:48 -0700

    Milestone 1

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,9 +1,22 @@
+// COMSC-210 | Lab 36 | Gillian Rhett
 #include <iostream>
 #include "IntBinaryTree.h"
 using namespace std;
+/*Milestones to document with your commits, in addition to 10-minute commits (commit must read 'Milestone n' where n is the milestone number):
+
+1. Project is set up and running using the provided files.
+2. Code modification from int to strings is complete, and your code exercises this for testing.
+3. Your code successfully creates a BST from the records and outputs it using the method of your choosing. 
+4. Your code's menu is functional.
+*/
 
 int main() {
-    
+    // make it work for strings rather than ints
 
+    // Write a program that creates and populates a BST from these records. 
+
+    // Add interactive, menu-driven functionality to add, delete, search, and modify records.
+
+  
     return 0;
 }
